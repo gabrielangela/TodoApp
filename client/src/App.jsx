426e1a6d8874f1022df9c00e4c833c29358a2ef9
gabrielangela <gabrielangelam29@gmail.com>
2025-07-08 13:27:46 +0700

    chore: test vercel deployment 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,24 @@
 import { useEffect } from 'react'
 import Home from './pages/Home'
 
-useEffect(() => {
-  async function fetchTodos() {
-    try {
-      const response = await fetch(
-        "https://jelly-thrilling-cartwheel.glitch.me/todos",
-        {
-          method: "GET",
-        }
-      );
-      const result = await response.json();
-      console.log(result);
-    } catch(error) {
-      console.log(error);
-    }
-  }
-  fetchTodos();
-}, [])
-
 function App() {
+  useEffect(() => {
+    async function fetchTodos() {
+      try {
+        const response = await fetch(
+          "https://jelly-thrilling-cartwheel.glitch.me/todos",
+          {
+            method: "GET",
+          }
+        );
+        const result = await response.json();
+        console.log(result);
+      } catch(error) {
+        console.log(error);
+      }
+    }
+    fetchTodos();
+  }, [])
   return <Home />
 }
 
